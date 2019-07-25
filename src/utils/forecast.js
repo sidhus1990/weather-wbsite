d716b4bb31a163b562b312524c28c1c3ccc392e6
Manpreet <m.singh@barneys.com>
2019-07-26 00:36:19 +0530

    added hih and low temp to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Please try another search.');
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree celcius. There is a '+ body.currently.precipProbability+'% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degree celcius. The high today is '+ body.daily.data[0].temperatureHigh +' with a low of '+ body.daily.data[0].temperatureHigh+'. There is a '+ body.currently.precipProbability+'% chance of rain.');
         }
     })
 }
